fa3142802988522cbc97bad7fc2c2973959d8e68
Ravi Kumar Nimmi <rvkmr102@gmail.com>
2015-10-17 00:20:03 +0530

    fetch_user_profile

--- whitewalkers/apis/views.py
+++ whitewalkers/apis/views.py
@@ -43,60 +43,62 @@ def send_questions(request):
 
 @csrf_exempt
 def fetch_user_profile(request):
-	data = request.POST
-	#uid = data['email_id']
-	# query_output  = mongo_query('uid')
-	response = HttpResponse(json.dumps({
+    data = request.POST
+    user_id = data['user_id']
+    gender = data['gender']
+    age = data['age']
+    user = User(user_id=user_id, gender=gender, age=age)
+    user.save()
+    response = HttpResponse(json.dumps({
             'status': 'success',
             'data': data
         }), content_type='application/json')
-	print data, '2'
-	response['Access-Control-Allow-Origin'] = '*'
-	return response
-	
+    response['Access-Control-Allow-Origin'] = '*'
+    return response
+
 
 def get_questions(request):
-	data = request.GET
-	uid = data['email_id']
-	#questions = mongo_query(age, gender)
-	questions = [{'question': 'GOT rocks?',
-		'template_id': 4,
-		'options': ['agree', 'disagree'],
-		'question_id': 10},
-		{'question': 'Friends rocks?',
-		'template_id': 4,
-		'options': ['agree', 'disagree'],
-		'question_id': 11},
-		{'question': 'Seinfeld rocks?',
-		'template_id': 4,
-		'options': ['agree', 'disagree'],
-		'question_id': 12},
-		{'question': 'House of cards rocks?',
-		'template_id': 4,
-		'options': ['agree', 'disagree'],
-		'question_id': 13},
-		{'question': 'Deathnote rocks?',
-		'template_id': 4,
-		'options': ['agree', 'disagree'],
-		'question_id': 14}
-	]
-	response = HttpResponse(json.dumps(questions))
-	return response
+    data = request.GET
+    uid = data['email_id']
+    #questions = mongo_query(age, gender)
+    questions = [{'question': 'GOT rocks?',
+        'template_id': 4,
+        'options': ['agree', 'disagree'],
+        'question_id': 10},
+        {'question': 'Friends rocks?',
+        'template_id': 4,
+        'options': ['agree', 'disagree'],
+        'question_id': 11},
+        {'question': 'Seinfeld rocks?',
+        'template_id': 4,
+        'options': ['agree', 'disagree'],
+        'question_id': 12},
+        {'question': 'House of cards rocks?',
+        'template_id': 4,
+        'options': ['agree', 'disagree'],
+        'question_id': 13},
+        {'question': 'Deathnote rocks?',
+        'template_id': 4,
+        'options': ['agree', 'disagree'],
+        'question_id': 14}
+    ]
+    response = HttpResponse(json.dumps(questions))
+    return response
 
 @csrf_exempt
 def get_response(request):
-	data = request.POST
-	print data, '1'
-	# import ipdb
-	# ipdb.set_trace()
-	response = HttpResponse(json.dumps({
+    data = request.POST
+    print data, '1'
+    # import ipdb
+    # ipdb.set_trace()
+    response = HttpResponse(json.dumps({
             'status': 'success',
             'data': data
         }), content_type='application/json')
-	print data, '2'
-	response['Access-Control-Allow-Origin'] = '*'
-	return response
-	
+    print data, '2'
+    response['Access-Control-Allow-Origin'] = '*'
+    return response
+    
 
 def get_templates(request):
     data = request.GET
